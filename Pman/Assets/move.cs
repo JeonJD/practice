07c242616dbce1e937d7e1e32e374eab472563e1
JeonJD <JeonJD@gmail.com>
2018-08-05 01:03:55 +0900

    move

--- Pman/Assets/move.cs
+++ Pman/Assets/move.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float speed;
-    Vector3 direction;
+
+    private Vector3 direction;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.)
+        if (Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) ||
+            Input.GetKey(KeyCode.DownArrow))
+        {
+            float xValue = Input.GetAxisRaw("Vertical");
+            float zValue = Input.GetAxisRaw("Horizontal");
+            direction = xValue * Vector3.forward + zValue * Vector3.right;
+
+            this.transform.rotation = Quaternion.LookRotation(direction);
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
     }
 }
